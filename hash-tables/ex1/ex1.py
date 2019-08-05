1da959ee5fec1429c9fab9507bb235864fbcfe89
pghrogue <jenbellpgh@gmail.com>
2019-08-04 22:59:37 -0400

    ex1 is mostly working, but I don't understand how they get (1,0) for the first test, when both hash to the same location

--- hash-tables/ex1/ex1.py
+++ hash-tables/ex1/ex1.py
@@ -8,11 +8,24 @@ from hashtables import (HashTable,
 
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
+    index = 0
 
-    """
-    YOUR CODE HERE
-    """
+    # Fill in the hash table
+    for weight in weights:    
+        hash_table_insert(ht, weight, index)
+        index += 1
 
+    for x in range(length - 1, 0, -1):
+        print(f"x: {x} limit {limit} - weight {weights[x]}")
+        diff = limit - weights[x]
+        print(f"Diff {diff}")
+        if diff > 0:
+            weightKey = hash_table_retrieve(ht, weights[x])
+            hayKey = hash_table_retrieve(ht, diff)
+            print(f"w: {weightKey} h: {hayKey}")
+            if weightKey is not None and hayKey is not None:
+                return(weightKey, hayKey)
+        
     return None
 
 
